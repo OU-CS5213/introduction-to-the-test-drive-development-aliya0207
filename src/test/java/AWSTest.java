c8534eed9cc6b175a98999d440586dde12a551bf
aliya0207 <aliyashaikh02@ou.edu>
2021-03-03 23:05:44 -0600

    folder created

---
+++ src/test/java/AWSTest.java
@@ -0,0 +1,178 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AWSTest {
+
+	private static final int FILLER_VALUE = Integer.MIN_VALUE;
+	private int[] original={1, 2, 3,4,5};
+	AWS originalAWS;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		 originalAWS = new AWS(this.original);
+	}
+
+	@Test
+	void testGetValues() {
+		
+  		int[] x= {1,2,3};
+		AWS aws = new AWS(x);
+		int[] result = aws.getValues();
+		assertEquals(result[0],x[0]);
+	}
+
+	@Test
+	void testSetValues() {
+	
+		int[] x= {1,2,3};
+		int[] y= {7,8,9};
+		AWS aws = new AWS(x);
+		aws.setValues(y);
+		int[] result=aws.getValues();
+		assertEquals(result[0],y[0]);
+		assertEquals(result[2],y[2]);
+		assertEquals(result.length,y.length);
+		
+	}
+
+	@Test
+	void testToString() {
+		int[] x= {7,8,9};
+		AWS aws = new AWS(x);
+		String expected = "AWS [values=[7, 8, 9]]";
+		String result= aws.toString();
+		assertEquals(result,expected);
+		
+
+	}
+
+	@Test
+	void testAWS() {
+		int[] expected = {1, 2, 3};
+		int[] x = {1, 2, 3};
+		AWS aws = new AWS(x);
+		x[1] = 100;
+		
+		int[] actual = aws.getValues();
+		assertEquals(actual[0], expected[0]);
+		assertEquals(actual[1], expected[1]);
+	}
+	
+	@Test
+	void testRemove() {
+		int[] x = {1, 2, 3};
+		AWS aws = new AWS(x);
+		
+		int value = aws.remove(-1);
+		int expected = FILLER_VALUE;
+		assertEquals(expected, value);
+		
+		 value = aws.remove(x.length + 10);
+		expected = FILLER_VALUE;
+		assertEquals(expected, value);
+		
+		value = aws.remove(0);
+		assertEquals(x[0], value);
+		
+		int[] r = aws.getValues();
+		value = r[r.length - 1];
+		assertEquals(expected, value);
+		
+		value = aws.remove(2);
+		assertEquals(r[2], value);
+		
+		r = aws.getValues();
+		value = r[r.length - 1];
+		assertEquals(expected, value);
+		
+	}
+	
+	@Test
+	void testFillAndExpand() {
+		int position = 1;
+		int numberOfTimes = 2;
+		int[] org = originalAWS.getValues();
+		int expectedValue = org[position];
+		int first = org[0];
+ 		
+		int expected = originalAWS.getValues().length + numberOfTimes;
+		originalAWS.fillAndExpand(position, numberOfTimes);
+		int[] result = originalAWS.getValues();
+		assertEquals(expected, result.length);
+		
+		int a = result[1];
+		int b = result[2];
+		int c = result[3];
+		assertEquals(expectedValue, a);
+		assertEquals(expectedValue, b);
+		assertEquals(expectedValue, c);
+		assertEquals(first, result[0]);
+		 
+		
+	
+	}
+	@Test
+	void testFillAndExpandWithNegative() {
+		int position = 1;
+		int numberOfTimes = -2;
+		
+		int[] org = originalAWS.getValues();
+		int expectedValue = org[position];
+ 		int first = org[0];
+		int expected = originalAWS.getValues().length + Math.abs(numberOfTimes);
+		originalAWS.fillAndExpand(position, numberOfTimes);
+		int[] result = originalAWS.getValues();
+		assertEquals(expected, result.length);
+		
+		int a = result[1];
+		int b = result[2];
+		int c = result[3];
+		assertEquals(expectedValue, a);
+		assertEquals(expectedValue, b);
+		assertEquals(expectedValue, c);
+		 
+		assertEquals(first, result[0]);
+
+	
+	}
+
+         @Test
+	 void testremoveBiggerThan()
+	 {
+		 //int[] x= {1,2,3,4,5};
+		 int threshold=3;
+		 int[] org = originalAWS.getValues();
+		 int expectedCount=2;
+		 int expected= FILLER_VALUE;
+				 
+		 int resultCount=originalAWS.removeBiggerThan(threshold);
+		 int[] resultValues=originalAWS.getValues();
+		
+		assertEquals(expectedCount,resultCount);
+		assertEquals(expected,resultValues[3]);
+		assertEquals(expected,resultValues[4]);
+
+		 
+	 }
+
+
+         @Test
+	void teststepMultiplier()
+	{
+		int [] x= {3,18,30};
+		AWS aws = new AWS(x);
+		int expectedA=3*2;
+		int expectedB=18*4;
+		int expectedC=30*100;
+		
+		aws.stepMultiplier();
+		int []result=aws.getValues();
+		assertEquals(expectedA, result[0]);
+		assertEquals(expectedB, result[1]);
+		assertEquals(expectedC, result[2]);
+		
+		
+	}
+}
